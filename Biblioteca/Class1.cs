4ac3f167917d5d793d19584c07af7eb0b08ba50c
leompf <lmfraqueiro@gmail.com>
2025-07-15 22:08:03 +0100

    Criação da classe e incorporação no Forms

---
+++ Biblioteca/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Biblioteca;
+
+public class Post
+{
+    private string _titulo;
+
+    private string _descricao;
+
+    private DateTime _data;
+
+    private int _gostos;
+
+    private bool _estado;
+
+    public string Titulo
+    {
+        get { return _titulo; }
+        set 
+        {
+            if (value != "")
+            _titulo = value; 
+        }
+    }
+
+    public string Descricao
+    {
+        get { return _descricao; }
+        set
+        {
+            if (value != "")
+                _descricao = value;
+        }
+    }
+
+    public int Gostos
+    {
+        get { return _gostos; }
+        set
+        {
+            _gostos = value;
+        }
+    }
+    public DateTime Data
+    {
+        get { return _data; }
+    }
+
+    public bool Estado
+    {
+        get { return _estado; }
+    }
+
+    public void DataPost()
+    {
+        _data = DateTime.Now;
+    }
+
+    public bool CriarPost()
+    {
+        _estado = false;
+
+        if (Titulo != null && Descricao != null)
+        {
+            _estado =  true;
+        }
+
+        return _estado;
+    }
+}
--- ExercicioPost.Forms/Form1.cs
+++ ExercicioPost.Forms/Form1.cs
@@ -1,10 +1,68 @@
-namespace ExercicioPost.Forms
+using Biblioteca;
+using System.ComponentModel.Design;
+
+namespace ExercicioPost.Forms;
+
+public partial class frmPost : Form
 {
-    public partial class Form1 : Form
+    private Post _post;
+
+    public frmPost()
     {
-        public Form1()
+        InitializeComponent();
+        _post = new Post();
+
+        if (_post != null)
         {
-            InitializeComponent();
+            grpboxCriarPost.Enabled = true;
         }
+    }
+
+    private void frmPost_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCriar_Click(object sender, EventArgs e)
+    {
+        _post.Titulo = tbTitulo.Text;
+        _post.Descricao = rtbDescricao.Text;
+
+        _post.CriarPost();
+
+        if (_post.Estado == true)
+        {
+            _post.DataPost();
+            lblData.Text = _post.Data.ToString("dd/MM/yyyy, HH:mm");
+            grpboxVerPost.Visible = true;
+            lblMensagem.Visible = false;
+
+            ApresentarPost();
+        }
+        else
+        {
+            lblMensagem.Text = "Por favor preencha todos os campos!";
+            lblMensagem.Visible = true;
+        }
+
+    }
+
+    private void btnGosto_Click(object sender, EventArgs e)
+    {
+        _post.Gostos++;
+        lblGostos.Text = _post.Gostos.ToString();
+    }
+
+    private void btnNaoGosto_Click(object sender, EventArgs e)
+    {
+        _post.Gostos--;
+        lblGostos.Text = _post.Gostos.ToString();
+    }
+
+    private void ApresentarPost()
+    {
+        textBox1.Text = _post.Titulo;
+        richTextBox1.Text = _post.Descricao;
+        label3.Text = _post.Data.ToString("dd/MM/yyyy, HH:mm");
     }
 }
